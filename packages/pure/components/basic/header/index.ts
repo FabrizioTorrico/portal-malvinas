ef3a2588aedf7d5d8c25697eee9110d6dc189dbc
FabrizioTorrico <fabriziotorrico27@gmail.com>
2025-07-16 22:56:39 -0300

    refactor controller

--- packages/pure/components/basic/header/index.ts
+++
@@ -1,3 +0,0 @@
-export type * from './types'
-export * from './utils'
-export { HeaderController } from './hooks'
--- packages/pure/components/basic/index.ts
+++ packages/pure/components/basic/index.ts
@@ -1,3 +1,3 @@
 export { default as Footer } from './Footer.astro'
-export { default as Header } from './Header.astro'
+export { default as Header } from './header/Header.astro'
 export { default as ThemeProvider } from './ThemeProvider.astro'
