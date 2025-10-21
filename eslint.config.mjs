0646dd7d41693ae755b16e30da759674db124493
FabrizioTorrico <fabriziotorrico27@gmail.com>
2025-10-20 22:02:25 -0300

    eslint only working on specific files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-
+import { Linter } from "eslint"
 import eslint from '@eslint/js'
 import eslintPluginAstro from 'eslint-plugin-astro'
 import tseslint from 'typescript-eslint'
 
+/** @type {(Linter.Config<Linter.RulesRecord> | { readonly rules: Readonly<Linter.RulesRecord>; } | Linter.FlatConfig)[]} */
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
